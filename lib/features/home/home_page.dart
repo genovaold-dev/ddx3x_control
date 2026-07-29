f9b58016e2f487b2cd736ceaaf727960451e9725
genovaold-dev <genovaold@gmail.com>
2026-07-29 16:46:35 +0200

    Preparazione progetto iOS e Android

--- lib/features/home/home_page.dart
+++ lib/features/home/home_page.dart
@@ -1,16 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+@override
+State<HomePage> createState() => _HomePageState();
+}
+ 
+class _HomePageState extends State<HomePage> {
+ late final WebViewController controller;
+@override
+ void initState() {
+    super.initState();
+
+    controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..loadRequest(Uri.parse('https://www.ddx3x.it'));
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text(
-          'DDX3X Home',
-          style: TextStyle(fontSize: 24),
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('DDX3X'),
+      ),
+      body: WebViewWidget(
+        controller: controller,
       ),
     );
   }
